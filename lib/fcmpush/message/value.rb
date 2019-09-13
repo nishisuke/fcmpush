b320fec721b80ab5b7f71e64d77f3d61404d9a35
nishisuke <ryoskns+git@gmail.com>
2019-09-13 15:24:24 +0900

    fix valu

--- lib/fcmpush/message/value.rb
+++ lib/fcmpush/message/value.rb
@@ -3,14 +3,27 @@
 module Fcmpush
   class Message
     class Value
+      class InvalidError < ::Fcmpush::Error
+        attr_reader :value
+
+        def initialize(value)
+          @value = value
+        end
+      end
+
       def initialize(**hash)
         hash.each_pair do |key, val|
           instance_variable_set("@#{key}", val)
         end
       end
 
+      def valid!
+        raise InvalidError.new(self) unless valid?
+        self
+      end
+
       def valid?
-        raise NotImplementedError
+        false
       end
     end
   end
