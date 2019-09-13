fb2759ab8b6c03704a3960fee861de251c1b01ea
nishisuke <ryoskns+git@gmail.com>
2019-09-13 17:08:36 +0900

    toke tpic

--- lib/fcmpush/message/token.rb
+++ lib/fcmpush/message/token.rb
@@ -3,6 +3,9 @@
 module Fcmpush
   class Message
     class Token < Value
+      def valid?
+        @token.instance_of?(String) || !@token.empty?
+      end
     end
   end
 end
--- lib/fcmpush/message/topic.rb
+++ lib/fcmpush/message/topic.rb
@@ -3,6 +3,11 @@
 module Fcmpush
   class Message
     class Topic < Value
+      def valid?
+        (@topic.instance_of?(String) || @topic.instance_of?(Symbol)) &&
+          !@topic.empty? &&
+          !@topic.to_s.start_with?('/topics/')
+      end
     end
   end
 end
